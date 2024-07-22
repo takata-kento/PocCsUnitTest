91d6e7ad7c543a8271eaa9261a40ab4c56471630
takata-kento <takata_kento@ims-sol.co.jp>
2024-07-23 01:38:47 +0900

    テスト対象コードのコミット

---
+++ SampleWPF/SampleWPF/ViewModels/ForwardedCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleWPF.ViewModels
+{
+    public class ForwardedCommand : ICommand
+    {
+        private Action action;
+        private Func<bool> canExecute;
+        public ForwardedCommand(Action a) : this(a, () => true) { }
+        public ForwardedCommand(Action a, Func<bool> cx)
+        {
+            action = a;
+            canExecute = cx;
+        }
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+#pragma warning disable CS8767
+        public bool CanExecute(object parameter)
+#pragma warning restore CS8767
+        {
+            return canExecute();
+        }
+#pragma warning disable CS8767
+        public void Execute(object parmeter)
+#pragma warning restore CS8767
+        {
+            action();
+        }
+    }
+
+    // Parameter 付き Command
+    public class ForwardedCommand<T> : ICommand
+    {
+        private Action<T> action;
+        private Func<bool> canExecute;
+        public ForwardedCommand(Action<T> a) : this(a, () => true) { }
+
+        public ForwardedCommand(Action<T> a, Func<bool> cx)
+        {
+            action = a;
+            canExecute = cx;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+#pragma warning disable CS8767
+        public bool CanExecute(object parameter)
+#pragma warning restore CS8767
+        {
+            return canExecute();
+        }
+
+#pragma warning disable CS8767
+        public void Execute(object parameter)
+#pragma warning restore CS8767
+        {
+            action((T)parameter);
+        }
+    }
+}
---
+++ SampleWPF/SampleWPF/ViewModels/MainWindowVM.cs
@@ -0,0 +1,109 @@
+﻿using SampleWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace SampleWPF.ViewModels
+{
+    public class MainWindowVM
+    {
+        public static ObservableCollection<User> users { get; set; }
+
+        public ObservableCollection<User> Users
+        {
+            get => users;
+            set
+            {
+                users = value;
+            }
+        }
+
+
+        /*
+        public static ObservableCollection<User>? userList { get; set; }
+        public ObservableCollection<User> UserList
+        {
+#pragma warning disable CS8603 // Null 参照戻り値である可能性があります。
+            get => userList;
+#pragma warning restore CS8603 // Null 参照戻り値である可能性があります。
+            set
+            {
+                userList = value;
+            }
+        }
+        */
+
+
+        // Insert用データ
+        private string insertFirstName;
+        public string InsertFirstName
+        {
+            get => insertFirstName;
+            set
+            {
+                insertFirstName = value;
+            }
+        }
+
+        private string insertLastName;
+        public string InsertLastName
+        {
+            get => insertLastName;
+            set
+            {
+                insertLastName = value;
+            }
+        }
+
+        private void UpdateUserList(User _user)
+        {
+            Users.Add(_user);
+        }
+
+        private void DropUserList(int _id)
+        {
+            Users.Remove(users.Where(user => user.Id == _id).First());
+        }
+
+        // ForwardedCommand
+        public ICommand InsertCommand { get; private set; }
+        public void ExecuteInsert()
+        {
+            // LocalDB 更新
+            User insertUser = new()
+                {
+                    // Id は LocalDB で自動的に付与される
+                    FirstName = InsertFirstName,
+                    LastName = InsertLastName
+                };
+
+            UpdateUserList(insertUser);
+        }
+
+        public ICommand ListTrashCommand { get; private set; }
+        public void ExecuteTrash(int _id)
+        {
+            // LocalDB 更新
+            DropUserList(_id);
+        }
+
+        public MainWindowVM()
+        {
+            Users = new ObservableCollection<User>();
+
+            InsertCommand = new ForwardedCommand(ExecuteInsert);
+            ListTrashCommand = new ForwardedCommand<int>(ExecuteTrash);
+
+            // CS8618 対応
+            insertFirstName = "";
+            insertLastName = "";
+
+        }
+    }
+}
